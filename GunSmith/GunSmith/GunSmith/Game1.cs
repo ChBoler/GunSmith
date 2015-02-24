0a8eed1597e5c10a165c7af3259bf474193325f9
ChBoler <chboler@hotmail.com>
2015-02-23 20:41:14 -0800

    Basic Functionality 1
    Added fullscreen, ability to exit with escape, a basic rendering class
    and a placeholder title graphic.

--- GunSmith/GunSmith/GunSmith/Game1.cs
+++ GunSmith/GunSmith/GunSmith/Game1.cs
@@ -1,3 +1,6 @@
+// ============================= Game1.cs =================================
+//       Main class of the game; all game logic starts out here.
+// ========================================================================
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,43 +14,54 @@ using Microsoft.Xna.Framework.Media;
 
 namespace GunSmith
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    // The main game method; logic all starts here
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        // Set the default width and height for the game
+        public static int windowWidth = 1920;
+        public static int windowHeight = 1080;
+
+        //TODO: Look into checking/changing resolution values
+        public static int currentWindowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public static int currentWindowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = windowWidth;
+            graphics.PreferredBackBufferHeight = windowHeight;
+
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        // Allows the game to perform any initialization it needs to before starting to run.
+        // This is where it can query for any required services and load any non-graphic
+        // related content.  Calling base.Initialize will enumerate through any components
+        // and initialize them as well.
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        // Publically declare various 'textures' so they can be accessed outside
+        // of this class by various rendering classes.
+        public static Texture2D TitleGraphic;
+
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Create a new SpriteBatch for each class that needs to render images.
+            //spriteBatch = new SpriteBatch(GraphicsDevice);
+            MainMenu.spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+
+            // TODO: Figure out if this can be loaded and unloaded in a seperate class
+
+            // Initiate Menu Graphics
+            TitleGraphic = Content.Load<Texture2D>("DebugTitleImage");
         }
 
         /// <summary>
@@ -66,11 +80,12 @@ namespace GunSmith
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
+            // Grab the state of the keys being used
+            KeyboardState currentKeyboardState = Keyboard.GetState();
 
-            // TODO: Add your update logic here
+            // Allows the game to exit
+            if (currentKeyboardState.IsKeyDown(Keys.Escape))
+                this.Exit();
 
             base.Update(gameTime);
         }
@@ -83,7 +98,8 @@ namespace GunSmith
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Display the main menu
+            MainMenu.RenderMenu();
 
             base.Draw(gameTime);
         }
---
+++ GunSmith/GunSmith/GunSmith/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿// ============================= MainMenu.cs =================================
+//  The games main menu; responsible for showing the intro screen, showing
+//   the different menu options, and initiating different game features.
+// ===========================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GunSmith
+{
+    public class MainMenu
+    {
+        // Handles image rendering
+        public static SpriteBatch spriteBatch;
+
+        // Renders the main menu of the game; displays menu choices in addition to
+        // handling menu controls and transitions.
+        public static void RenderMenu()
+        {
+            // Render a test graphic
+            spriteBatch.Begin();
+            spriteBatch.Draw(Game1.TitleGraphic, 
+                new Vector2(Game1.currentWindowWidth / 2, Game1.currentWindowHeight / 15), 
+                null, Color.White, 0, new Vector2(Game1.TitleGraphic.Width / 2, 0), 1, 
+                SpriteEffects.None, 0);
+            spriteBatch.End();
+        }
+    }
+}
